ffe62c95d3d5d61c1d555bd303b6ddbf567a4391
angel <angel.sanzam@educa.jcyl.com>
2025-11-12 17:03:40 +0100

    Clase 11/11/25

--- src/Primer_trimestre/Tema_2/Pruebas/PruebaArrayAleatorio.java
+++ src/Primer_trimestre/Tema_2/Pruebas/PruebaArrayAleatorio.java
@@ -6,12 +6,10 @@ import java.util.Scanner;
 public class PruebaArrayAleatorio {
     public static void main(String[] args) {
         Random r=new Random();
-        r.nextInt(10);
         Scanner scanner= new Scanner(System.in);
         System.out.println("dime un numero");
-        int[]numeroArray=r.nextInt(10);
-        for (int i=0;i<numeroArray.length;i++){
 
-        }
+
+
     }
 }
