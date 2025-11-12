6583d59513f3809f1f3b438af90fdbc0deeb295a
angel <angel.sanzam@educa.jcyl.com>
2025-11-12 17:04:02 +0100

    Clase 11/11/25

---
+++ src/Primer_trimestre/Tema_2/Pruebas/ArrayDeArrays.java
@@ -0,0 +1,22 @@
+package Primer_trimestre.Tema_2.Pruebas;
+
+import java.util.Arrays;
+
+public class ArrayDeArrays {
+    public static void main(String[] args) {
+        String[] datosF = {"Felipe", "Garcia", "Avda. La Palmera 1"};
+        String[] datosL = {"Laura", "Rodriguez", "Calle del Chopo 43"};
+        String[] datosA = {"Abraham", "Moreno", "El grande S/N"};
+
+        System.out.println(Arrays.toString(datosF));
+        System.out.println(Arrays.toString(datosL));
+        System.out.println(Arrays.toString(datosA));
+
+        String[][] alumnos = {datosF, datosL, datosA};
+//        alumnos = new String[3][3];
+        System.out.println("Vamos a imprimir el array de arrays");
+        for (String[] alumno : alumnos) {
+            System.out.println(Arrays.toString(alumno));
+        }
+    }
+}
---
+++ src/Primer_trimestre/Tema_2/Pruebas/ArrayDeArrays2.java
@@ -0,0 +1,12 @@
+package Primer_trimestre.Tema_2.Pruebas;
+
+public class ArrayDeArrays2 {
+    public static void main(String[] args) {
+        int[][] numeros=new int[3][2];
+
+
+        for (int i; i<3;i++){
+            sou
+        }
+    }
+}
